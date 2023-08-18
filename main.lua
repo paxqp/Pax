e3abf9c44df819a1c6e7e03bc0180febc66c97b3
paxqp <84926133+paxqp@users.noreply.github.com>
2023-08-18 13:38:57 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,28 @@
 local plr = game.Players.LocalPlayer
 
 local manufactures = {game.Workspace.Manufacture1, game.Workspace.Manufacture2, game.Workspace.Manufacture3, game.Workspace.Manufacture4}
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("onlybarbarian Made It :)", "All")
+local messages = {"onlybarbarian on dizzy", "Need cash, add me on dizzy onlybarbarian.", "Don't off me, I am doing nothing to you", "TOO FAST!!!", "Boop", "onlybarbarian is the best coder"}
+
+-- ANTI AFK
+local virtualUser = game:service'VirtualUser'
+
+-- Connect a function to the 'Idled' event of the player. This function runs when Roblox detects idling.
+game.Players.LocalPlayer.Idled:connect(function() virtualUser:CaptureController() virtualUser:ClickButton2(Vector2.new()) end)
+
 
 local function manuGrab()
 	local manu = manufactures[math.random(#manufactures)] 
 	if manu.UIProgressPART.PrintingPROGRESS.MAINUI.Background.LOADING.Size == UDim2.new(0,0,1,0) then return manu end
 	manuGrab()
+end
+
+local function selling()
+	print("Selling")
+	fireproximityprompt(game:GetService("Workspace").ManufactureSeller1.SellToggle)
+	wait(2)
+	if game.Players.LocalPlayer.Backpack:FindFirstChild("BriefcaseManufacture") ~= nil then
+		selling()
+	end
 end
 
 local function main()
@@ -20,6 +36,7 @@ local function main()
 	
 	-- Turn On Machine
 	fireclickdetector(game:GetService("Workspace").clickdoors.MoneyManufacture.MoneyManufactureENTER.ClickDetector)
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(messages[math.random(#messages)] , "All")
 	wait(2)
 	char:MoveTo(manu.ActivateBUTTON.Position)
 	wait(0.5)
@@ -42,6 +59,7 @@ local function main()
 	
 	-- Collect Cash
 	wait(40)
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(messages[math.random(#messages)] , "All")
 	
 	if char == nil or char.Humanoid.Health <1 then return end
 	fireclickdetector(game:GetService("Workspace").clickdoors.MoneyManufacture.MoneyManufactureENTER.ClickDetector)
@@ -61,6 +79,7 @@ local function main()
 	wait(0.15)
 	char:MoveTo(manufactures[math.random(#manufactures)].ToggleABLES.CollectCashPART.Position)
 	wait(0.2)
+	char:MoveTo(manufactures[math.random(#manufactures)].ToggleABLES.CollectCashPART.Position)
 	
 	-- Sell
 	if char == nil or char.Humanoid.Health <1 then return end
@@ -73,19 +92,26 @@ local function main()
 	char:MoveTo(manufactures[math.random(#manufactures)].ToggleABLES.CollectCashPART.Position)
 	wait(0.15)
 	char:MoveTo(manufactures[math.random(#manufactures)].ToggleABLES.CollectCashPART.Position)
-	wait(0.15)
+	wait(0.1)
+	char:MoveTo(manufactures[math.random(#manufactures)].ToggleABLES.CollectCashPART.Position)
+	wait(0.1)
 	char:MoveTo(manufactures[math.random(#manufactures)].ToggleABLES.CollectCashPART.Position)
 	
-	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false task.wait(2) game.Players.LocalPlayer.Character:MoveTo(game:GetService("Workspace").MoneyManufactureBUYER.HumanoidRootPart.Position)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false task.wait(2) game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-783.9000244140625, 3.0349996089935303, -392.6236267089844))
 	if char == nil or char.Humanoid.Health <1 then return end
 	wait(1)
-	fireproximityprompt(game:GetService("Workspace").ManufactureSeller1.SellToggle)
-	wait(3)
+	
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(messages[math.random(#messages)] , "All")
+	selling()
 end
 
 while true do
     local success, err = pcall(main)
-    if not success then
+ 	if not success then
         print(err)  -- print the error to the output
     end
 end
+
+
+
+pcall(main)
