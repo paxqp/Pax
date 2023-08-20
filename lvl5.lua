01c30136a29373e08955799d8cb986b85c96d289
paxqp <84926133+paxqp@users.noreply.github.com>
2023-08-20 02:23:04 -0700

    Update lvl5.lua

--- lvl5.lua
+++ lvl5.lua
@@ -25,6 +25,11 @@ wait(0.25)
 plr.Character:MoveTo(CJ.Interaction.Position)
 wait(0.25)
 plr.Character:MoveTo(game:GetService("Workspace")["Electric box"].Union.Position)
+wait(0.25)
+plr.Character:MoveTo(CJ.Interaction.Position)
+wait(0.25)
+plr.Character:MoveTo(game:GetService("Workspace").labtabellong.Part.Position)
+wait(0.1)
 
 fireclickdetector(game:GetService("Workspace").clickdoors.AccessorySHOP.OutletENTER.ClickDetector)
 
