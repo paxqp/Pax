72bb3e5def92c2971b358ba3e29369bbe37ed774
paxqp <84926133+paxqp@users.noreply.github.com>
2023-08-20 02:16:15 -0700

    Create lvl5.lua

---
+++ lvl5.lua
@@ -0,0 +1,64 @@
+local doors = game:GetService("Workspace").clickdoors
+local plr = game.Players.LocalPlayer
+
+-- Enter
+local CJ = game:GetService("Workspace"):FindFirstChild("CookingJob")
+if CJ.Interaction.progress.progressback.mainprogress.Size ~= UDim2.new(0,0,0,15) then return end
+
+fireclickdetector(doors.cookjob1oblock.CookJob1Oblock.ClickDetector)
+wait(2)
+
+plr.Character:MoveTo(CJ.Interaction.Position)
+wait(0.25)
+fireproximityprompt(CJ.Interaction.CookingInteraction)
+wait(0.25)
+
+-- Hide
+plr.Character:MoveTo(game:GetService("Workspace").labtabellong.Part.Position)
+wait(0.25)
+plr.Character:MoveTo(CJ.Interaction.Position)
+wait(0.25)
+plr.Character:MoveTo(game:GetService("Workspace")["Electric box"].Union.Position)
+wait(0.25)
+plr.Character:MoveTo(game:GetService("Workspace").labtabellong.Part.Position)
+wait(0.25)
+plr.Character:MoveTo(CJ.Interaction.Position)
+wait(0.25)
+plr.Character:MoveTo(game:GetService("Workspace")["Electric box"].Union.Position)
+
+fireclickdetector(game:GetService("Workspace").clickdoors.AccessorySHOP.OutletENTER.ClickDetector)
+
+wait(60)
+
+-- Grab Ba
+plr.Character:MoveTo(CJ.Interaction.Position)
+wait(0.25)
+
+----------
+-- Sell --
+----------
+
+fireclickdetector(doors.apartment1leave.ClickDetector)
+
+-- Dodge
+plr.Character:MoveTo(game:GetService("Workspace").labtabellong.Part.Position)
+wait(0.25)
+plr.Character:MoveTo(CJ.Interaction.Position)
+wait(0.25)
+plr.Character:MoveTo(game:GetService("Workspace")["Electric box"].Union.Position)
+wait(0.25)
+plr.Character:MoveTo(game:GetService("Workspace").labtabellong.Part.Position)
+wait(0.25)
+plr.Character:MoveTo(CJ.Interaction.Position)
+wait(0.25)
+plr.Character:MoveTo(game:GetService("Workspace")["Electric box"].Union.Position)
+
+
+wait(2)
+plr.Character:MoveTo(CJ.Interaction.Position)
+
+
+
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(game.Workspace.mapshopbuttons.sellbakingsoda.Position.X, game.Workspace.mapshopbuttons.sellbakingsoda.Position.Y, game.Workspace.mapshopbuttons.sellbakingsoda.Position.Z - 3))
+wait(1)
+fireproximityprompt(game.Workspace.mapshopbuttons.sellbakingsoda.sellbakingsoda)
